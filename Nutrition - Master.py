f76d42ae35d6375a1637fe2f843564c6e9cc048f
FrancoBoni <32057824+FrancoBoni@users.noreply.github.com>
2017-09-28 20:44:27 +0200

    Update Nutrition
    +lunch While True

--- Nutrition - Master.py
+++ Nutrition - Master.py
@@ -1,11 +1,10 @@
+from NutritionTheory import *
+
 print("Hi! welcome to \"Cheff Malony!\" - The first chat bot that suggests meals based on your calories to burn. "
       "like a nutrition doctor.\nPlease, answer the following 4 questions in order to know your calories to burn.\n\n ")
 men_calories_to_burn = 0
 women_calories_to_burn = 0
 calories_to_burn = 0
-
-from FoodMenuOptions import *
-
 
 #validate Gender
 while True:
@@ -20,14 +19,6 @@ while True:
         print("Didn't hear of that gender yet.")
 
 
-#validate age
-def validAge(age):
-    try:
-        age = int(age)
-        return 1 <= age <= 115
-    except ValueError:
-        return False
-
 while True:
     Age = input("Please enter your age > ")
     if validAge(Age) is True:
@@ -36,14 +27,6 @@ while True:
     else:
         print("That can't be right, can it?")
 
-
-#validate height
-def validHeight(cm):
-    try:
-        cm = int(cm)
-        return 40 <= cm <= 250
-    except ValueError:
-        return False
 
 while True:
     Height = input("Please enter your height in centimetres(e.g.:172) > ")
@@ -54,14 +37,6 @@ while True:
         print("Are you that tall? come on...")
 
 
-#validate weight
-def validWeight(Weight):
-    try:
-        Weight = int(Weight)
-        return 1 <= Weight <= 300
-    except ValueError:
-        return False
-
 while True:
     Weight = input("Please enter your weight in kilograms as a whole number (e.g.:78) > ")
     if validWeight(Weight) is True:
@@ -71,18 +46,9 @@ while True:
         print("I don't know if i am nor wearing my glasses today but that doesn't seem like your weight, am i right?")
 
 
-#calorie count
 WeightForCalorie = int(Weight)
 AgeForCalorie = int(Age)
 HeightForCalorie = int(Height)
-
-def men_calories_calculator(weight,height,age):
-    men = 10*weight + 6.25*height - 5*age + 5
-    return men
-
-def woman_calories_calculator(weight,height,age):
-    woman = 10*weight + 6.25*height - 5*age - 161
-    return woman
 
 if gender == "M":
     men_calories_to_burn = men_calories_calculator(WeightForCalorie,HeightForCalorie,AgeForCalorie)
@@ -94,7 +60,8 @@ else: pass
 
 
 #startin with your breackfast
-print("\n\nSo, let's get started. We will be segguesting different meals for your day ingest.\n ")
+print("\n\nSo, let's get started. We will be segguesting different meals for your day ingest."
+        "Each option is based on a basic serving size but you can order it twice if you love it!\n ")
 
 #Creating a class for food in order to store more than 1 value.
 # Source: https://whatscookingamerica.net/NutritionalChart.htm
@@ -106,45 +73,26 @@ if women_calories_to_burn != 0:
 calories_left = round(calories_to_burn)
 
 
-
-import csv
-
-class Food:
-    def __init__(self, name, calorie, serving_size, healthiness):
-        self.name = name
-        self.calorie = calorie
-        self.serving_size = serving_size
-        self.healthiness = healthiness
-
-
 breakfast_foods = []
-def open_a_menu_csv(file):
-    with open(file) as foods:
-        dict_reader = csv.DictReader(foods)
-        for row in dict_reader:
-            food = Food(row['name'], int(row['calorie']), row['serving_size'], row['healthiness'])
-            breakfast_foods.append(food)
-
-open_a_menu_csv('foodfb.csv')
+open_a_menu_csv('foodfb.csv',breakfast_foods)
+breakfast_of_the_day = ""
 
 print("Breakfast Menu:")
 for food in breakfast_foods:
     print(food.name,"("+food.serving_size+"):", food.calorie, "calories")
 
-
 #for each food in your food list check if the food.name is equal to the input string
 
 
-breakfast_of_the_day = ""
 while True:
     #breakfastime!
-    bf_choice = input("What are you having for breakfast? (if you want to go to the next meal type 'next meal'> ").lower()
+    bf_choice = input("Current Breakfast:What are you having for breakfast? (if you want to go to the next meal type 'next meal'> ").lower()
     if bf_choice == "next meal":
-        nobreakfast = False
+        no_breakfast = False
         if breakfast_of_the_day == "":
             breakfast_of_the_day = "Nothing"
-            nobreakfast = True
-        print("Your current day ingestion:", breakfast_of_the_day)
+            no_breakfast = True
+        print("Your current breakfast ingestion:", breakfast_of_the_day)
         satisfied = input("Are you satisfied with your current breakfast(say 'no' if you want to do over)").lower()
         if satisfied == "no":
             breakfast_of_the_day = ""
@@ -162,3 +110,51 @@ while True:
         if not found:
             print("Not a valid breakfast choice")
 
+
+
+lunch_foods = []
+open_a_menu_csv('foodfl.csv',lunch_foods)
+lunch_of_the_day = ""
+
+print("\nSo! Starting to get hungry no? These are the food categories for your lunch meal:")
+
+while True:
+    #lunchtime!
+    segm_choice = input("Choose a food category to fill you in with food options"
+                           "(if you want to go to the next meal type 'next meal')> ")
+    if segm_choice == "next meal":
+        no_lunc = False
+        if lunch_of_the_day == "":
+            lunch_of_the_day = "Nothing"
+            no_lunch = True
+        print("Your current lunch ingestion:", lunch_of_the_day)
+        satisfied = input("Are you satisfied with your current lunch(say 'no' if you want to do over)").lower()
+        if satisfied == "no":
+            lunch_of_the_day = ""
+            calories_left = round(calories_to_burn)
+        elif satisfied != "no":
+            break
+    else:
+        found = False
+        for food in lunch_foods:
+            if food.segment == segm_choice:
+                print(food.name,"("+food.serving_size+"):", food.calorie, "calories")
+                found = True
+                if not found:
+                    print("Not a valid lunch choice")
+        if found:
+            found = False
+            lunch_choice = input("What's your choice for lunch?")
+            for food in lunch_foods:
+                if food.name == lunch_choice:
+                    calories_left = calories_left - food.calorie
+                    lunch_of_the_day = lunch_of_the_day + " - " + lunch_choice
+                    print("You have", calories_left,
+                          "left to burn. Do you want to have anything else or just:" + lunch_of_the_day, "?")
+                    found = True
+        if not found:
+            print("not a valid category")
+
+
+menu_of_the_day = breakfast_of_the_day + lunch_of_the_day
+print("These is your current menu of the day:", menu_of_the_day)
