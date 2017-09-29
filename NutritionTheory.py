8d1ce5219369a451e850fdd4175fece1c3d36e42
FrancoBoni <32057824+FrancoBoni@users.noreply.github.com>
2017-09-29 12:28:23 +0200

    Update NutritionTheory.py

--- NutritionTheory.py
+++ NutritionTheory.py
@@ -54,12 +54,23 @@ def open_a_menu_csv(file,menu_foods):
 def calorie_limit(calorie_balance):
     if -200 <= calorie_balance <= 200:
         print("Is about to round things up because you have", calorie_balance, "calories balance.")
-    elif calorie_balance < -200:
+    if -400 < calorie_balance < -201:
         print("\nWe suggest you to close your mouth darling. Your current calorie count is:", calorie_balance,
               "\nOr you can do over your meal but definetely not having more to eat.")
         stop = True
+    if calorie_balance < -401:
+        print("Come on you're killing me. ")
     else:pass
 
+def recommendation_warning(the_input,the_string):
+    if the_input == the_string:
+        print("We don't recommend eating",the_string,"at night but is your call...")
 
-segm_choice = ['alcohols','meats','breads','cheeses','chocolates','condiments','oils','fruits','seeds','lambs',
+def healthy_warning(menu_of_the_day):
+    for food in menu_of_the_day:
+        if food.healthiness is "healthy" > food.healthiness is "not healthy":
+            print("Watch out with what you are eating, ")
+
+
+segments = ['alcohols','meats','breads','cheeses','chocolates','condiments','oils','fruits','seeds','lambs',
                'pastas','porks','chickens','fishes','vegetables']
